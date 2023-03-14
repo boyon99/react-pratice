224d6b4812f60f75e9de376563c8d1e04f713a1c
boyon99 <bo_yon_99@naver.com>
2023-03-14 21:51:00 +0900

    :memo: feat: Add chapter 6 컴포넌트 반복 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-// 경로 : src/App.js
-import React, { Component, useState, useEffect } from "react";
-import "./App.css";
-
-function App() {
-  return (
-    <>
-    
-    </>
-    )
+import React, { Component } from "react";
+import IterationSample from "./IterationSample";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <IterationSample />
+      </div>
+    );
+  }
 }
-
 export default App;
-
