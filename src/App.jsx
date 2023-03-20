90668b0e321a07d70c649a28c1cab66eb4712342
boyon99 <bo_yon_99@naver.com>
2023-03-20 13:06:23 +0900

    :memo: Update Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import IterationSample from "./IterationSample";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div>
-        <IterationSample />
+        <div className="IterationSample">
+          <IterationSample />
+        </div>
       </div>
     );
   }
