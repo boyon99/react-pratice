1c3ea35dc6a22f482c7975eea65384af91b440c4
boyon99 <bo_yon_99@naver.com>
2023-03-21 22:47:07 +0900

    :recycle: feat: Reset code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import React, { Component } from "react";
-import IterationSample from "./6/IterationSample";
-import LifeCycleSample from "./7/LifeCycleSample";
-import "./App.scss";
+import React from 'react';
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
+const App = () => {
+  return <div>hello</div>;
+};
 
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <div className="IterationSample">
-          <IterationSample />
-        </div>
-        <div className="LifeCycleSample">
-          <button onClick={this.handleClick}>랜덤색상</button>
-          <LifeCycleSample color={this.state.color} />
-        </div>
-      </div>
-    );
-  }
-}
 export default App;
