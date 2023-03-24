4cfa5d90de1bc7be09cd0add871f830c125f78f5
boyon99 <bo_yon_99@naver.com>
2023-03-24 14:58:02 +0900

    :hammer: feat: Delete store/title

--- src/shop/pages/Cart.js
+++ src/shop/pages/Cart.js
@@ -1,17 +1,13 @@
 import React, { } from 'react';
 import { Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
-import { setName } from '../store/title';
 import { setStock } from '../store/stock';
 
 const Cart = () => {
   let stock = useSelector((state) => { return state.stock })
-  let title = useSelector((state) => { return state.title })
   let dispatch = useDispatch()
   return (
     <>
-      <h1>{title}</h1>
-      <button onClick={() => { dispatch(setName()) }}>name</button>
       <Table>
         <thead>
           <tr>
--- src/shop/store.js
+++ src/shop/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import stock from './store/stock';
-import title from './store/title';
 
 export default configureStore({
   reducer: {
     // 등록하기
     stock: stock.reducer,
-    title: title.reducer
   }
 }) 
