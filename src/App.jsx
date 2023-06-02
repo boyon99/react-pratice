567bd30c4bb234b71aab1c90d99f2b1dc4fbe7a6
boyon99 <bo_yon_99@naver.com>
2023-06-02 19:09:55 +0900

    :bulb: feat: Add color-thief-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+// import ColorThiefComponent from './library/ColorThief';
 // redux
 // import Shop from './shop/App';
 // import Chapter from './17/App';
@@ -12,6 +13,7 @@ const App = () => {
         {/* <Shop /> */}
         {/* <Chapter /> */}
         {/* <Counter /> */}
+        {/* <ColorThiefComponent /> */}
       </div>
       <div className="hello">Hello React</div>
     </div>
